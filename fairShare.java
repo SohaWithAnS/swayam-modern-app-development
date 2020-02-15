7894b20491ddf8fa4e326f8308af6ab2b9734d26
Soha <36818003+SohaWithAnS@users.noreply.github.com>
2020-02-15 15:09:47 +0530

    Update fairShare.java

--- fairShare.java
+++ fairShare.java
@@ -5,26 +5,30 @@ import java.text.*;
 
 public class fairShare
 {
-    /*String[] inputCommands --> for storing the 3 user commands
+    /*String[] inputCommands --> for storing the 3 types of user commands
     1. registering of new roommates
     2. storing expenses of each event
     3. generating report for a particular individual
     */
-    public String[] inputCommands; 
-    public String[] Roommates;
-    public int noOfEvents;
-    public Map<String, Double> fairShare;
+    public String[] inputCommands;
+    public String[] Roommates; //for registering roommates
+    public int noOfEvents; //for counting the number of events (if required, for debugging)
+    public Map<String, Double> fairShare; //for storing fair share of each roommate (updated after each event)
     
+    //Initialization of map that will contain updated fair share of each roomate
     public void initializeFairShareList()
     {
         fairShare = new HashMap<String,Double>();
         
         for(int i=0; i<Roommates.length; i++)
         {
-            fairShare.put(Roommates[i],0.0);
+            fairShare.put(Roommates[i],0.00);
         }
     }
 
+    /*if roommate name is known, to get index of the roommate.
+    if roommate is not registered, returns back -1
+    */
     public int getIndexOfRoommate(String name)
     {
         for(int i=0; i<Roommates.length; i++)
@@ -37,18 +41,20 @@ public class fairShare
         return -1;
     }
 
+    /*for debugging
+    used to display the updated fair share of each roommate
+    gets updated after every event
+    */
     public void displayExpenses()
     {
-        /*for(int j = 0; j<fairShare.size(); j++)
-        {
-            System.out.println(fairShare.get(j));
-        }*/
         System.out.println(fairShare);
     }
 
+    //used to register new roommates
     public void Register(String toRegister)
     {  
-        final String[] r = toRegister.split(" ");
+        String reg = toRegister.replaceAll(" +", " ");
+        String[] r = reg.split(" ");
         
         //excluding "register" while storing, hence starting with index "1"
         Roommates = Arrays.copyOfRange(r,1, r.length);
@@ -59,11 +65,13 @@ public class fairShare
         }*/
     }
 
+    //used to add new event and update the fair share of each roommate
     public void addExpense(String toAddExpense)
-    {
-        String[] e = toAddExpense.split(" ");
-
-        /*for(final String i : e)
+    {   
+        String reg = toAddExpense.replaceAll(" +", " ");
+        String[] e = reg.split(" ");
+        
+        /*for(String i : e)
         {
             System.out.println(i);
         }*/
@@ -72,20 +80,18 @@ public class fairShare
         
         if(indexOfRoommate!=-1) //if roommate is registered
         {
-            final double perHeadShare = Double.valueOf(e[2])/Roommates.length;
+            double perHeadShare = Double.valueOf(e[2])/Roommates.length;
             //System.out.println(perHeadShare);
             
             for(int i=0; i<Roommates.length; i++)
             {
                 if(i==indexOfRoommate)
                 {
-                    //System.out.println("Hi");
                     double old = fairShare.get(Roommates[i]);
                     fairShare.put(Roommates[i], old+(Double.valueOf(e[2])-perHeadShare));
                 }
                 else
                 {
-                    //System.out.println("Hello");
                     double old = fairShare.get(Roommates[i]);
                     fairShare.put(Roommates[i], old+(-perHeadShare));
                 }
@@ -101,30 +107,42 @@ public class fairShare
     
     }
 
+    //used to generate report (final fair share) of a particular roommate
     public void generateReport(String toGenerateReport)
     {
-        String roommate = toGenerateReport.split(" ")[1];
+        String reg = toGenerateReport.replaceAll(" +", " ");
+        String roommate = reg.split(" ")[1];
 
         //System.out.println(fairShare.get(roommate));
         //System.out.println("Rounded off : "+ Round(fairShare.get(roommate)));
-        System.out.println(Round(fairShare.get(roommate)));
+        if(getIndexOfRoommate(roommate)!=-1)
+        {
+            //System.out.println(Round(fairShare.get(roommate)));
+            System.out.printf("%.2f%n", fairShare.get(roommate));
+            //"%,.2d%n" if we want commas like 30,066,253,287.00 instead of 30066253287.00
+
+        }
+        else
+        {
+            System.out.println("Error");
+        }
     }
 
     public void decideMode(String input)
     {
-        //initializeFairShareList();
-
-        //splitting commands by tab and storing them in a String[]
-        inputCommands = input.split("   ");
+        //splitting commands by comma and storing them in a String[]
+        inputCommands = input.split(",");
         
-        for(final String command : inputCommands)
+        for(String command : inputCommands)
         {   
+            //replacing multiple spaces with a single space
+            String reg = command.replaceAll(" +", " ");
+
             //splitting every individual command by space, getting first element (which is the mode of operation)
-            final String mode = command.split(" ")[0];
-            
+            String mode = reg.split(" ")[0];
+
             if(mode.equalsIgnoreCase("register"))
             {
-                //System.out.println("register mode");
                 Register(command);
                 initializeFairShareList();
             }
@@ -132,25 +150,21 @@ public class fairShare
             {
                 noOfEvents++;
                 addExpense(command);
-                //System.out.println(noOfEvents);
-                //System.out.println("expense mode");
             }
             else if(mode.equalsIgnoreCase("report"))
             {
                 generateReport(command);
-                //System.out.println("report mode");
             }
         }
         
     }
 
-    public double Round(double a)
+    /*public Double Round(double a)
     {
-        //double time = 200.3456;
-        DecimalFormat df = new DecimalFormat("#.##");      
+        DecimalFormat df = new DecimalFormat("#,##0.00");      
         a = Double.valueOf(df.format(a)); 
         return a;  
-    }
+    }*/
 
     public void execute(String input)
     {
@@ -167,7 +181,7 @@ public class fairShare
         while(scanner.hasNextLine())
         {
             line = scanner.nextLine();
-            input += line + "   "; //tab added as a separator (for splitting the commands)
+            input += line + ","; //comma added as a separator (for splitting the commands)
 
             if(line.equalsIgnoreCase("end"))
             {
@@ -183,3 +197,4 @@ public class fairShare
         obj.execute(input);
     }
 }
+
